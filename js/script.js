eb77f2d931d871c28c71e251a6fadeabf55f5c91
Luiz Carlos <luiz.carlosdev@outlook.com>
2024-02-15 10:12:25 -0300

    alt

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 function tabuada() {
     const num = document.querySelector("#num");
-    const seltab = document.querySelector("#seltab");
+    const seltab = document.querySelector("#select");
     seltab.textContent = " ";
     if(Number(num.value) == " ") {
         alert("Defina algum algarismo para prosseguir.")
     } else {
         for(let s = 0; s <= 10; s++) {
-            const item = document.createElement("option");
+            const item = document.createElement("li");
             item.textContent += `${Number(num.value)} x ${s} = ${Number(num.value)*s}`;
             seltab.appendChild(item);
         }
     }
 
     num.value = " ";
-    num.focus();
 }
